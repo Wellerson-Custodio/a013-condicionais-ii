d43726c278ec58f6620ac956fdb2fa34c8215117
Wellerson-Custodio <wellerson.custodio@gmail.com>
2022-08-10 18:29:30 -0300

    exercicios feitos.

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,23 @@
-const num = //seu numero aqui
+const num = +prompt("Indique um numero!")
+if(num % 2 === 0) {
+    console.log(num, "divisivel por 2")
+    if(num % 3 === 0){
+console.log(" O numero ", num, "é divisivel 3.");
+    }
+}else if(num % 2 === 0 && num % 3){
+    console.log(num? "divisivel por 2 e 3" : "não é divisivel por 2 e 3");
+   
+}else if(!num % 2 === 0){
+    console.log(num, "não é divisivel por 2");
 
+}else if(!num % 3 === 0){
+    console.log(num, "nao é divisivel por 3")
+}
+
+
+// const num1 = +prompt( "indique um numero!")
+// if(num1 % 2 === 0 || num1 % 3 === 0) {
+   
+//     console.log( "o número é divisível por 2 ou 3.")
+// }else
+// console.log(num1 % 2 === 0 || num1 % 3 === 0? "é divisível por 2  3" : "não é divisível por 2 e 3.")
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,10 @@
 //código a ser reescrito
-
+const nome = prompt("Qual é o nome?")
 if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
+	console.log(nome === "José"? "Oi, Zé!": "Olá", nome);
+
 }
 
 if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+	console.log(idade >= 18 ?"pode tirar carteira de motorista!" :"Ainda não pode tirar carteira de motorista" );
+} 
